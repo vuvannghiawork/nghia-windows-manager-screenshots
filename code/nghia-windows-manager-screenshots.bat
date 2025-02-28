582dd7c164e245f4e385fb4a8bf1cd6b1f20bdb9
Vu Van Nghia <vuvannghia.work@gmail.com>
2025-02-28 14:00:37 +0700

    nghia-git-auto-commit 2025_02_28_14_00_37_578592

--- code/nghia-windows-manager-screenshots.bat
+++ code/nghia-windows-manager-screenshots.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 
-python C:\Users\Admin\Desktop\link\NghiaDownloads\NghiaGit\NghiaGitlab\whynotnghiavu\os\windows\nghia-windows-manager-screenshots\code\nghia-windows-manager-screenshots.py
+python   C:\Users\Admin\Desktop\NghiaGithub\nghia-windows-manager-screenshots\code\nghia-windows-manager-screenshots.py
 
 endlocal
